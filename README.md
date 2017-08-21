ab64101c6b4ef8c05f3f0ff0338aa038fde2e0c1
Andrey Ovcharenko <an6rey@gmail.com>
2017-08-22 02:53:33 +0300

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+book
+====
+
+A Symfony project created on August 21, 2017, 11:33 pm.
---
+++ src/MyBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MyBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/my")
+     */
+    public function indexAction()
+    {
+        return $this->render('MyBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/about/{name}", name="aboutpage")
+     */
+     public function aboutAction($name)
+     {
+          return $this->render('AppBundle:about:index.html.twig', array('name' =>
+            $name));
+     }
+}
